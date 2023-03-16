76d54dc17c9927a671c7ff057bd8101e090e1a6f
Mohammad <mohammadhprp@gmail.com>
2023-03-16 13:17:19 +0330

    fix: add storage url path

--- lib/constants/url.dart
+++ lib/constants/url.dart
@@ -2,5 +2,9 @@ import 'package:flutter/foundation.dart' show immutable;
 
 @immutable
 class Url {
+  static const url = 'https://github.com/mohammadhprp/green-market/blob/master';
+
+  static const storage = '$url/storage';
+  static const images = '$storage/images';
   const Url._();
 }
