4ab7bfada941630801da7f672c77ff39ef2029f3
Mohammad <mohammadhprp@gmail.com>
2023-03-16 14:18:25 +0330

    feat: add app icons path

--- lib/constants/app/app_icons.dart
+++ lib/constants/app/app_icons.dart
@@ -4,5 +4,13 @@ import 'package:flutter/foundation.dart' show immutable;
 class AppIcons {
   static const _path = 'assets/icons';
 
+  static const cartAlt = "$_path/bx_cart-alt.svg";
+  static const heart = "$_path/bx_heart.svg";
+  static const leftArrowAlt = "$_path/bx_left-arrow-alt.svg";
+  static const messageSquareDots = "$_path/bx_message-square-dots.svg";
+  static const search = "$_path/bx_search.svg";
+  static const shoppingBag = "$_path/bx_shopping-bag.svg";
+  static const sliderAlt = "$_path/bx_slider-alt.svg";
+
   const AppIcons._();
 }
