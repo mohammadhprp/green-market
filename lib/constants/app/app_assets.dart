0d0b77b9801f2a56e750ba2271bc912cc2265c01
Mohammad <mohammadhprp@gmail.com>
2023-03-19 18:08:08 +0330

    feat: add cached_network_image package

--- lib/constants/app/app_assets.dart
+++ lib/constants/app/app_assets.dart
@@ -4,8 +4,8 @@ import 'package:flutter/foundation.dart' show immutable;
 class AppAssets {
   static const _images = 'assets/images';
 
-  static const onBoard1 = '$_images/on-board-1.png';
-  static const splash = '$_images/splash.png';
+  static const onBoard1 = '$_images/on-board-1.webp';
+  static const splash = '$_images/splash.webp';
 
   const AppAssets._();
 }
