14657c56a119c75c5c8d733021f7edc3bf517f17
Mohammad <mohammadhprp@gmail.com>
2023-03-19 13:59:58 +0330

    feat: update app values

--- lib/constants/themes/font_manager.dart
+++ lib/constants/themes/font_manager.dart
@@ -20,6 +20,7 @@ class FontWeightManager {
 
 class FontSize {
   static const double s12 = 12.0;
+  static const double s13 = 13.0;
   static const double s14 = 14.0;
   static const double s16 = 16.0;
   static const double s17 = 17.0;
